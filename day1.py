a20e910004461dadc1b07bd454c0845937bec19d
JwwccR <1638542844@qq.com>
2026-06-24 12:37:05 +0800

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,13 @@
+# Python 基础：变量和类型
+name = "数据挖掘"
+year = 2026
+skills = ["Python", "Pandas", "PyTorch", "推荐系统"]
+info = {"学校": "西南石油大学", "方向": "数据挖掘", "目标": "成都20万+"}
+
+print(name, year)
+print("要学的技能：", skills)
+print("我的信息：", info)
+
+# 列表操作
+skills.append("因果推断")
+print("加一个差异化技能：", skills)
